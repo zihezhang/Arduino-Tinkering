ed03d6cdfaba32cf534931e1d29c2df378d94acd
Zihe Zhang <51214897+zihezhang@users.noreply.github.com>
2021-05-06 11:25:15 -0400

    Parking help program

---
+++ Parking_Helper/Parking_Helper.ino
@@ -0,0 +1,89 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+const int led1 = 2, led2 = 3, led3 = 4, led4 = 5, led5 = 6;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  
+  // Sets LEDs as Output
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  // Turns on all 5 LEDs when car is <45cm away
+  if (distance < 45){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,HIGH);
+    digitalWrite(led5,HIGH);
+  }
+  // Turns on first 4 LEDs when car is 45-60cm away
+  else if (distance > 45 && distance < 60){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,HIGH);
+    digitalWrite(led5,LOW);
+  }
+  // Turns on first 3 LEDs when car is 60-80cm away
+  else if (distance > 60 && distance < 80){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+  }
+  // Turns on 2 LEDs when car is 80-105cm away
+  else if (distance > 80 && distance < 105){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+  }
+  // Turns on 1 LED when car is 105-135cm away
+  else if (distance > 105 && distance < 135){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+  }
+  // All LEDs are off when car is >125cm away
+  else if (distance > 135){
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+  }
+  delay(50);
+}
